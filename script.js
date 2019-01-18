2027feb307bbd690cbd2c729a6b8a7a27cd24de1
Karina <karyna.selivanova@nure.ua>
2019-01-18 23:04:49 +0200

    refactoring code

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 'use strict';
+
 const roles = ['Городничий', 'Аммос Федорович', 'Артемий Филиппович', 'Лука Лукич'];
-const textLine = 'Городничий. Я пригласил вас, господа, с тем чтобы сообщить вам пренеприятное известие: к нам едет ревизор.\n' +
-    'Аммос Федорович. Как ревизор?\n' +
-    'Артемий Филиппович. Как ревизор?\n' +
-    'Городничий. Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.\n' +
-    'Аммос Федорович. Вот те на!\n' +
-    'Артемий Филиппович. Вот не было заботы, так подай!\n' +
-    'Лука Лукич. Господи Боже! еще и с секретным предписаньем!';
-const arrReplics = textLine.split('\n');
+const textLine = `Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.
+Аммос Федорович: Как ревизор?
+Артемий Филиппович: Как ревизор?
+Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.
+Аммос Федорович: Вот те на!
+Артемий Филиппович: Вот не было заботы, так подай!
+Лука Лукич: Господи боже! еще и с секретным предписаньем!`;
+const arrReplicas = textLine.split('\n');
 const play = document.getElementById('play');
-const innerPlay = ()=>{
-    for(let i = 0;i<=roles.length-1;i++){
-        const p = document.createElement('p');
-        play.appendChild(p);
-        p.innerText = roles[i] + ':\n';
-        for(let j = 0; j<=arrReplics.length-1;j++){
-            if(arrReplics[j].indexOf(roles[i]) !== -1){
-                const p = document.createElement('p');
-                play.appendChild(p);
-                p.innerText = (j+1) +') ' + arrReplics[j].slice(roles[i].length + 2);
-            }
-        }
-    }
-}
-window.onload = ()=>{
+
+const innerPlay = () => {
+   roles.forEach(role => {
+       const p = document.createElement('p');
+       play.appendChild(p);
+       p.innerText = role + ':\n';
+       arrReplicas.forEach((replica, index) => {
+           if (replica.indexOf(role) !== -1) {
+               const p = document.createElement('p');
+               play.appendChild(p);
+               p.innerText = (index + 1) + ') ' + replica.slice(role.length + 2);
+           }
+       })
+   });
+};
+
+window.onload = () => {
     innerPlay()
-}
+};
